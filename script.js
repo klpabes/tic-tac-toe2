dc1de4eb76be8c1e7f2a6d5b23feb7faff25e3cc
klpabes <121295353+klpabes@users.noreply.github.com>
2025-04-18 17:05:01 +0800

    Add cell grid with eventListeners, move handler, putSymbol functions

--- script.js
+++ script.js
@@ -1,114 +1,79 @@
 "use strict";
 
 const gameBoard = (function () {
+  const playButton = document.querySelector("#play-button");
+  const cellCount = 9;
+  const gameBoard = document.querySelector(".game-board");
+  let currentPlayer;
+  let playing;
+
+  function handleMove(cell) {
+    if (cell.textContent || !playing) return;
+    if (cell.textContent === "") {
+      cell.textContent = currentPlayer;
+      putSymbol(currentPlayer, cell.dataset.index);
+      if (checkWinCondition(currentPlayer)) {
+        playing = !playing;
+        playButton.textContent = "Play Again?";
+      }
+    }
+
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  }
+
+  function displayCells() {
+    gameBoard.innerHTML = "";
+    currentPlayer = "X";
+    for (let i = 0; i < cellCount; i++) {
+      let cell = document.createElement("div");
+      cell.setAttribute("class", "cell-grid");
+      cell.setAttribute("data-index", `${i}`);
+      gameBoard.appendChild(cell);
+    }
+
+    const cells = document.querySelectorAll(".cell-grid");
+
+    cells.forEach((cell) => {
+      cell.addEventListener("click", function (e) {
+        handleMove(cell);
+      });
+    });
+  }
+
   let board;
 
-  const putX = function (num) {
-    if (num < 9 && !board[num]) {
-      return (board[num] = "X");
-    }
-    console.log("Cell Taken");
-  };
-
-  const putO = function (num) {
-    if (num < 9 && !board[num]) {
-      return (board[num] = "O");
-    }
-    console.log("Cell Taken");
+  const putSymbol = function (symbol, num) {
+    board[num] = symbol;
   };
 
   const play = function () {
+    playing = true;
     board = new Array(9);
-    let playing = true;
-    while (playing) {
-      let winner;
-
-      if (!winner) {
-        let xNumber = Number(prompt("Put X from 0 - 9"));
-        putX(xNumber);
-        winner = checkWinCondition("X");
-      }
-
-      if (!winner) {
-        let oNumber = Number(prompt("Put O from 0 - 9"));
-        putO(oNumber);
-        winner = checkWinCondition("O");
-      }
-
-      if (winner) {
-        console.log("Yes");
-        console.log(`Player ${winner} Won!!`);
-        playing = !playing;
-      }
-
-      //check closures
-    }
+    displayCells();
+    playButton.textContent = "Reset";
   };
 
-  // 012
-  // 345
-  // 678
-  // 036
-  // 147
-  // 258
-  // 048
-  // 246
+  playButton.addEventListener("click", () => play());
 
   const checkWinCondition = function (symbol) {
-    if (board[0] == symbol && board[1] == symbol && board[2] == symbol) {
-      console.log("U Win!!");
+    if (board[0] == symbol && board[1] == symbol && board[2] == symbol)
       return symbol;
-    } else if (board[3] == symbol && board[4] == symbol && board[5] == symbol) {
-      console.log("U Win!!");
+    else if (board[3] == symbol && board[4] == symbol && board[5] == symbol)
       return symbol;
-    } else if (board[6] == symbol && board[7] == symbol && board[8] == symbol) {
-      console.log("U Win!!");
+    else if (board[6] == symbol && board[7] == symbol && board[8] == symbol)
       return symbol;
-    } else if (board[0] == symbol && board[3] == symbol && board[6] == symbol) {
-      console.log("U Win!!");
+    else if (board[0] == symbol && board[3] == symbol && board[6] == symbol)
       return symbol;
-    } else if (board[1] == symbol && board[4] == symbol && board[7] == symbol) {
-      console.log("U Win!!");
+    else if (board[1] == symbol && board[4] == symbol && board[7] == symbol)
       return symbol;
-    } else if (board[2] == symbol && board[5] == symbol && board[8] == symbol) {
-      console.log("U Win!!");
+    else if (board[2] == symbol && board[5] == symbol && board[8] == symbol)
       return symbol;
-    } else if (board[0] == symbol && board[4] == symbol && board[8] == symbol) {
-      console.log("U Win!!");
+    else if (board[0] == symbol && board[4] == symbol && board[8] == symbol)
       return symbol;
-    } else if (board[2] == symbol && board[4] == symbol && board[6] == symbol) {
-      console.log("U Win!!");
+    else if (board[2] == symbol && board[4] == symbol && board[6] == symbol)
       return symbol;
-    }
   };
-
-  //   const checkWinConditionO = function () {
-  //     if (board[0] == "O" && board[1] == "O" && board[2] == "O") {
-  //       console.log("U Win!!");
-  //       return "O";
-  //     } else if (board[3] == "O" && board[4] == "O" && board[5] == "O") {
-  //       console.log("U Win!!");
-  //       return "O";
-  //     } else if (board[6] == "O" && board[7] == "O" && board[8] == "O") {
-  //       console.log("U Win!!");
-  //       return "O";
-  //     } else if (board[0] == "O" && board[3] == "O" && board[6] == "O") {
-  //       console.log("U Win!!");
-  //       return "O";
-  //     } else if (board[1] == "O" && board[4] == "O" && board[7] == "O") {
-  //       console.log("U Win!!");
-  //       return "O";
-  //     } else if (board[2] == "O" && board[5] == "O" && board[8] == "O") {
-  //       console.log("U Win!!");
-  //       return "O";
-  //     } else if (board[0] == "O" && board[4] == "O" && board[8] == "O") {
-  //       console.log("U Win!!");
-  //       return "O";
-  //     } else if (board[2] == "O" && board[4] == "O" && board[6] == "O") {
-  //       console.log("U Win!!");
-  //       return "O";
-  //     }
-  //   };
-
   return { play };
 })();
+
+// gameBoard.play();
